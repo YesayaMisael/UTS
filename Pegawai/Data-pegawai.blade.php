5ae4e26dad82115ec0a3ef932161ca51708120e9
YesayaMisael <yesaya.misael21@student.esaunggul.ac.id>
2022-05-24 02:09:37 +0700

    project pertama

---
+++ Pegawai/Data-pegawai.blade.php
@@ -0,0 +1,72 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <td>Tabel TEMA DATA DOSEN</td>
+    <table border="1">
+        <tr>
+            <td>No</td>
+            <td>NAMA</td>
+            <td>Jenis Kelamin</td>
+            <td>Golongan Darah</td>
+            <td>Jabatan Fungsional</td>
+            <td>Pendidikan Terakhir</td>
+            <td>Universitas</td>
+            <td>Bidang ilmu / Spesialisasi</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Mawar Elizabet</td>
+            <td>Perempuan</td>
+            <td>AB</td>
+            <td>Dosen</td>
+            <td>S3</td>
+            <td>Universitas Indonesia</td>
+            <td>Kesenian</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Hidayat</td>
+            <td>Laki - laki</td>
+            <td>A</td>
+            <td>Asisten Dosen</td>
+            <td>S1</td>
+            <td>Univesitas Harapan Indah</td>
+            <td>Elektro</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Michael Ciesa</td>
+            <td>Laki - laki</td>
+            <td>O</td>
+            <td>Dosen</td>
+            <td>S3</td>
+            <td>Universitas Esa Unggul</td>
+            <td>Matematika</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Putri</td>
+            <td>Perempuan</td>
+            <td>O</td>
+            <td>Dosen</td>
+            <td>S2</td>
+            <td>Universitas Gunadarma</td>
+            <td>Biologi</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>John Picky</td>
+            <td>Laki - laki</td>
+            <td>B</td>
+            <td>Asisten Dosen</td>
+            <td>S1</td>
+            <td>Universitas Trisakti</td>
+            <td>Teknik Mesin</td>
+        </tr>
+    </table>
+</body>
+</html>
